847e10c34b38145c94e23a26ccc11e4cf0bd2404
Anton Karavikou <antontenix@gmail.com>
2021-11-13 17:45:34 +0300

    Code has been formatted

--- src/main/java/com/solvd/championship/Main.java
+++ src/main/java/com/solvd/championship/Main.java
@@ -12,12 +12,12 @@ public class Main {
         Root root = parser.parse();
         System.out.println("Root " + root.toString());
 
-        System.out.println("----------------------------JAXB----------------------------");
+        System.out.println("----------------------------JAXB---------------------------");
 
         MyJaxb jaxb = new MyJaxb();
         Root root2 = jaxb.parse();
 
-        System.out.println("---------------------------JackSON--------------------------");
+        System.out.println("--------------------------JackSON--------------------------");
 
         MyJsonJackson jackson = new MyJsonJackson();
         Root root3 = jackson.parse();
